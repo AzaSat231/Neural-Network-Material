72a49baf1d80dcbdc1a7d48c91ad45a5cf8cff06
Aziz Sattarov <azizsattarov231@gmail.com>
2025-12-15 10:43:59 +0200

    Modified cost_function, now it give totalCost value of NN

--- handwritten-recognition/main.py
+++ handwritten-recognition/main.py
@@ -9,8 +9,35 @@ import pickle
 def sigmoid(z):
     return 1.0/(1.0+np.exp(-z))
 
-def cost_function():
-    val = 1
+def cost_function(net_value):
+    amount_net = len(net_value[1])
+
+    total_cost = 0
+
+    for i in range(amount_net):
+        imageValue = np.array(net_value[0][i])
+
+        imageValue = imageValue.reshape(784, 1)    
+
+        # print(f"Value {i}: {imageValue.shape}")
+
+        net_output = net.feedforward(imageValue)
+
+        # print(f"Actual number {i}: {net_value[1][i]}")
+
+        desired_output = np.zeros((10, 1), dtype=int)
+
+        desired_output[net_value[1][i]] = 1
+
+        # print(f"Desired output: {desired_output}")
+
+        cost_result = pow((desired_output - net_output),2)
+
+        total_cost += cost_result
+
+    return (total_cost/(amount_net * 2)).sum()
+
+        
 
 # Representation of neural network, it initialization
 class Network(object):
@@ -25,14 +52,14 @@ class Network(object):
 
     def feedforward(self, a):
         """Return the output of the network if "a" is input."""
-        print(f"Bias: {self.biases}")
-        print(f"Weight: {self.weights}")
+        # print(f"Bias: {self.biases}")
+        # print(f"Weight: {self.weights}")
         for i, (b, w) in enumerate(zip(self.biases, self.weights)):
-            print(f"\nLayer {i+1}:")
-            print(f"Bias in loop: {b}")
-            print(f"Weight in loop: {w}")
+            # print(f"\nLayer {i+1}:")
+            # print(f"Bias in loop: {b}")
+            # print(f"Weight in loop: {w}")
             a = sigmoid(np.dot(w, a)+b)
-            print(f"a: {a}")
+            # print(f"a: {a}")
         
         return a
 
@@ -44,24 +71,8 @@ if __name__ == "__main__":
     with gzip.open("/Users/azizsattarov/Desktop/Federated Learning/neural-networks-and-deep-learning/data/mnist.pkl.gz", "rb") as f:
         train_set, valid_set, test_set = pickle.load(f, encoding="latin1")
 
-    imageValue = np.array(train_set[0][0])
+    val = cost_function(train_set)
 
-    imageValue = imageValue.reshape(784, 1)    
-
-    print(f"Value: {imageValue.shape}")
-
-    net_output = net.feedforward(imageValue)
-
-    print(f"Actual number: {train_set[1][0]}")
-
-    desired_output = np.zeros((10, 1), dtype=int)
-
-    desired_output[train_set[1][0]] = 1
-
-    print(f"Desired output: {desired_output}")
-
-    cost_result = pow((desired_output - net_output),2)
-
-    print(f"Cost of number {train_set[1][0]}: {cost_result.sum()}")
+    print(f"Total cost: {val}")
 
     
