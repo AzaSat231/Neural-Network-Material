25886ae72189d93f7ec794912ab61d15658cb46f
Aziz Sattarov <azizsattarov231@gmail.com>
2025-12-22 20:44:52 +0200

    Added training functionality with backpropagation logic, with clear comments

--- hadnwritten-recognition-pyTorch/main.py
+++ hadnwritten-recognition-pyTorch/main.py
@@ -9,6 +9,11 @@ class NeuralNetwork(nn.Module):
         self.flatten = nn.Flatten()
         # nn.Sequential is an ordered container of modules. 
         # The data is passed through all the modules in the same order as defined.
+        # In this nn.Linear we gonna have weights, biases and gradient. It will look like:
+        """ model.weight.data   # actual values
+            model.weight.grad   # gradient (after backward) 
+            model.layer1.weight.grad  # now contains ∂loss/∂W1
+            model.layer1.bias.grad """
         self.linear_relu_stack = nn.Sequential(
             nn.Linear(28*28, 512),
             nn.ReLU(),
@@ -22,7 +27,53 @@ class NeuralNetwork(nn.Module):
         logits = self.linear_relu_stack(x)
         return logits
 
+def train_loop(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    # Set the model to training mode - important for batch normalization and dropout layers
+    # Unnecessary in this situation but added for best practices
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        # Compute prediction and loss
+        # Forward pass — no gradients yet
+        pred = model(X)
+        loss = loss_fn(pred, y)
 
+        # Backpropagation, so it applies the chain rule backward for every parameter in the graph.
+        # So it will not store gradients into loss, it will store it model parameters in nn.Linear in our class, parameter.grad
+        # model.weight.data - actual values
+        # model.weight.grad - gradient (after backward)
+        loss.backward()
+
+        # Actual learning happening here, so it calculate our new new weights and biases
+        # based on the loss parameters that was calculated
+        optimizer.step()
+
+        # To not sum up gradients from before echo we need to initialise it
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * batch_size + len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+def test_loop(dataloader, model, loss_fn):
+    # Set the model to evaluation mode - important for batch normalization and dropout layers
+    # Unnecessary in this situation but added for best practices
+    model.eval()
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+
+    # Evaluating the model with torch.no_grad() ensures that no gradients are computed during test mode
+    # also serves to reduce unnecessary gradient computations and memory usage for tensors with requires_grad=True
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 
 
@@ -30,6 +81,7 @@ if __name__ == "__main__":
     transform = transforms.ToTensor()
     
     # Load dataset, into two sets: trainset, testset
+    # It will not download it again, if it already exist
     mnist_trainset = datasets.MNIST(
         root="/Users/azizsattarov/Desktop/Federated Learning",
         train=True,
@@ -45,93 +97,32 @@ if __name__ == "__main__":
     )
 
     # Initialise what the size of minibatches will be 64
-    train_dataloader = DataLoader(mnist_trainset, batch_size=64, shuffle=False)
-    test_dataloader = DataLoader(mnist_testset, batch_size=64, shuffle=False)
+    train_dataloader = DataLoader(mnist_trainset, batch_size=64)
+    test_dataloader = DataLoader(mnist_testset, batch_size=64)
 
     # Create our training model
     model = NeuralNetwork()
-    # print(model)
+
+    learning_rate = 1e-3
+    batch_size = 64
+    epochs = 5
+
+    # Initialize the loss function (same as cost function, both calculate same thing)
+    loss_fn = nn.CrossEntropyLoss()
+
+    # Optimizer store references to our weights and biases. 
+    # Not copies - actual references to nn.Parameter objects inside your model.
+    # So you can change weights and biases inside optimizer and it automatically will be changed in our model
+    # optimizer → [W1, b1, W2, b2, ...]
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+    epochs = 30
+    for t in range(epochs):
+        print(f"Epoch {t+1}\n-------------------------------")
+        train_loop(train_dataloader, model, loss_fn, optimizer)
+        test_loop(test_dataloader, model, loss_fn)
+    print("Done!")
 
 
 
 
-
-
-
-
-
-
-
-
-
-    # ----------------------------------------------------------
-    # Explnation of how Neural Network class work and intialised
-    # ----------------------------------------------------------
-
-    input_image = torch.rand(3,28,28)
-    # print(input_image.size())
-
-    # We use flatten to convert our 2D 28x28 into image into a contiguous array of 784 pixel values 
-    flatten = nn.Flatten()
-    flat_image = flatten(input_image)
-    # print(flat_image.size())
-
-    # The linear layer is a module that applies a linear transformation on the input using its stored weights and biases
-    layer1 = nn.Linear(in_features=28*28, out_features=20)
-    # Here it initialises all biases and weights. 
-    # It doing this: input nodes  ──(weights W)──▶ hidden nodes
-    hidden1 = layer1(flat_image)
-    print(hidden1.size())
-
-    """ Non-linear activations are what create the complex mappings between the model’s inputs and outputs. 
-        They are applied after linear transformations to introduce nonlinearity, helping neural networks learn a wide variety of phenomena.
-        In easier words it just apply this ReLU(x)=max(0,x). 
-        So now it will do this: Input → [ Linear (W, b) ] → [ ReLU ] → Output """
-    # print(f"Before ReLU: {hidden1}\n\n")
-    hidden1 = nn.ReLU()(hidden1)
-    # print(f"After ReLU: {hidden1}")
-
-    # nn.Sequential is an ordered container of modules. 
-    # The data is passed through all the modules in the same order as defined. 
-    # You can use sequential containers to put together a quick network like seq_modules.
-    seq_modules = nn.Sequential(
-        flatten,
-        layer1,
-        nn.ReLU(),
-        nn.Linear(20, 10)
-    )
-    input_image = torch.rand(3,28,28)
-    logits = seq_modules(input_image)
-
-
-
-
-
-    # ----------------------------------------------------------------------
-    # Explnation of how backpropogation work in pyTorch using torch.autograd
-    # ----------------------------------------------------------------------
-
-    x = torch.ones(5)  # input tensor
-    y = torch.zeros(3)  # expected output
-    w = torch.randn(5, 3, requires_grad=True)
-    b = torch.randn(3, requires_grad=True)
-    z = torch.matmul(x, w)+b
-    loss = torch.nn.functional.binary_cross_entropy_with_logits(z, y)
-
-    """ This object knows how to compute the function in the forward direction, 
-        and also how to compute its derivative during the backward propagation step. 
-        A reference to the backward propagation function is stored in grad_fn property of a tensor. """
-    print(f"Gradient function for z = {z.grad_fn}")
-    print(f"Gradient function for loss = {loss.grad_fn}")
-
-    """ By default, all tensors with requires_grad=True are tracking their computational history and support gradient computation. 
-        However, there are some cases when we do not need to do that, for example, 
-        when we have trained the model and just want to apply it to some input data, 
-        i.e. we only want to do forward computations through the network.
-        So we can disable it by torch.no_grad() """
-    z = torch.matmul(x, w)+b
-    print(z.requires_grad)
-
-    with torch.no_grad():
-        z = torch.matmul(x, w)+b
-    print(z.requires_grad)
